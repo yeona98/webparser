39df33ebd4c833a26207942f409b73261f73e529
yeona98 <sy4454@naver.com>
2019-01-16 16:40:31 +0900

    first commit

---
+++ parser.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","webparser.settings")
+
+import django
+django.setup()
+
+from data.models import WebData
+
+def parse_web():
+    # BASE_DIR = os.path.dirname(os.path.abspath(_file_))
+
+    url = 'http://www.koreaherald.com/'
+    req = requests.get(url)
+    # connect this url.
+
+    html= req.text
+    soup =  BeautifulSoup(html, 'html.parser')
+    titles = soup.select(
+        'dd > a'
+    )
+    # print(titles)
+
+    data = {}
+    for title in titles:
+        data[title.text] = title.get('href')
+        #print(title.text)
+        #print(data[title.text])
+    
+    return data
+    # print(soup)
+
+if __name__ == '__main__':
+    web_data_dict = parse_web()
+
+    url = 'http://www.koreaherald.com/'
+
+    for t, l in web_data_dict.items():
+        WebData(title=t, link=url+l).save()
